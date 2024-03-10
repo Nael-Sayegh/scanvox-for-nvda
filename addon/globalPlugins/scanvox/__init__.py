e25a78c7722b17813759fd6a798b16968eab5596
Nael-Sayegh <naelousay@gmail.com>
2024-03-10 11:35:54 +0100

    Add a message when thee scan is finish when automaticaly„ read text is disable

--- addon/globalPlugins/scanvox/__init__.py
+++ addon/globalPlugins/scanvox/__init__.py
@@ -178,6 +178,11 @@ class Scanvox(wx.Dialog):
 				text = ''.join(lines[lastIndex:-2])
 				if config.conf["scanvox"]["automaticalyReadText"]:
 					core.callLater(0, lambda: ui.message(text.replace("\n"," ")))
+			else:
+				ui.message(
+					# Translators: a message that the scan is complete
+					_("Scan complete")
+				)
 			self.contentText.AppendText(text+separator)
 			self.contentText.SetInsertionPoint(0)
 			self.on_Enable_Button(None)
