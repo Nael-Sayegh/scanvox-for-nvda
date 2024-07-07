531ce5c66650c970f0a7f5181ad8d5c75230dc81
Nael-Sayegh <naelousay@gmail.com>
2024-07-07 14:52:22 +0200

    Fixe name of methode GetKeyCode

--- addon/globalPlugins/scanvox/__init__.py
+++ addon/globalPlugins/scanvox/__init__.py
@@ -361,7 +361,7 @@ class Text:
 		for page in self.start:
 			if pos < page:
 				self.control.SetInsertionPoint(page)
-				if not evt.getkeycode() == wx.WXK_PAGEUP:
+				if not evt.GetKeyCode() == wx.WXK_PAGEUP:
 					core.callLater(
 						0, lambda: speakMessage(self.control.GetRange(page, page + 6))
 					)
@@ -381,7 +381,7 @@ class Text:
 		for page in reversed(self.start):
 			if pos > page:
 				self.control.SetInsertionPoint(page)
-				if not evt.getkeycode() == wx.WXK_PAGEDOWN:
+				if not evt.GetKeyCode() == wx.WXK_PAGEDOWN:
 					core.callLater(
 						0, lambda: speakMessage(self.control.GetRange(page, page + 6))
 					)
